31bdba1c2acaff7f3c94581c9c74df0764a1529a
rajmat <rajmund.matus@gmail.com>
2019-04-01 15:08:31 +0200

    a4

--- greet.js
+++ greet.js
@@ -1,12 +1,4 @@
 'use strict';
-<<<<<<< HEAD
 function greet () {
   console.log('Greetings');
 };
-=======
-function greet (names) {
-  console.log(names);
-};
-
-greet('Max and Rajmund');
->>>>>>> b9750ba86983358f5dafd9a33d83ab9a1b744e2b
